b1b3123b539ee6cc0d25e5e0aad6da1bd6411c77
Fauzansyz <fauzanadzma2@gmail.com>
2026-03-25 14:06:46 +0000

    Improve hero & fixing bug and adding badge in hero

---
+++ src/components/badges/BadgesComponents.jsx
@@ -0,0 +1,16 @@
+export default function BadgesComponents() {
+  return (
+    <>
+    <div className="p-1">
+    <span class="inline-flex items-center justify-center rounded-full bg-teal-200 px-2.5 py-0.5 text-teal-700 dark:bg-teal-300 dark:text-teal-700">
+      <p class="text-sm whitespace-nowrap">Website</p>
+    </span>
+
+    <span class="inline-flex items-center justify-center rounded-full border border-teal-500 px-2.5 py-0.5 text-purple-700 dark:text-[var(--text-h)]">
+      <p class="text-sm whitespace-nowrap">SEO</p>
+
+    </span>
+    </div>
+    </>
+  )
+}
--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -1,19 +1,35 @@
+import BadgesComponents from "../badges/BadgesComponents"
+
 export default function HeroSection() {
   return (
     <>
-    <div className="w-full p-5">
-    <span class="rounded-full bg-teal-100 px-3.5 py-0.5 text-sm whitespace-nowrap text-purple-700 dark:bg-teal-500 dark:text-white">
-      Live
-    </span>
+    <section class="bg-[var(--color-bg)] lg:grid lg:h-screen lg:place-content-center">
+      <div class="mx-auto w-screen max-w-7xl px-5 py-18 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
+        <div class="mx-auto max-w-prose text-center">
+          <h1 class="text-4xl font-bold text-[var(--text-h)] sm:text-6xl">
+            Understand user flow and
+            <strong class="text-teal-600"> increase </strong>
+            conversions
+          </h1>
+    <BadgesComponents />
 
-    <span class="rounded-full border border-teal-500 px-3.5 py-0.5 text-sm whitespace-nowrap text-purple-700 dark:text-white">
-      Live
-    </span>
-    </div>
-    <div className="w-full flex justify-center">
-    <h1 className="text-4xl w-90 ">lorem ipsum dolor sit
-    </h1>
-    </div>
+          <p class="mt-4 text-base text-pretty text-[var(--text-h)] sm:text-lg/relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, nisi. Natus, provident
+            accusamus impedit minima harum corporis iusto.
+          </p>
+
+          <div class="mt-4 flex justify-center gap-4 sm:mt-6">
+            <a class="inline-block rounded border border-teal-700 bg-teal-700 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700" href="#">
+              Get Started
+            </a>
+
+            <a class="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900" href="#">
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
     </>
   )
 
