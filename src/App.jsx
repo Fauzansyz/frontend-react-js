e762689a5c03193f3f7bd16a370545ab54ad05cb
Fauzansyz <fauzanadzma2@gmail.com>
2026-03-26 14:43:16 +0000

    adding pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,27 @@ import { useGSAP } from '@gsap/react'
 import './App.css'
 import TopNavBar from './components/navbar/TopNavBar'
 import HeroSection from './components/hero/HeroSection'
+import PricingSection from './components/pricing/PricingSection'
 import FeatureSection from './components/feature/FeatureSection'
 
 function App() {
   const [count, setCount] = useState(0)
   const ref = useRef(null)
 
+  const pricing = {
+    pricing: '$29',
+    feature: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.',
+    ]
+  }
+
   return (
     <>
     <TopNavBar />
-      <HeroSection />
-    <FeatureSection />
+     <HeroSection />
+      <FeatureSection />
+    <PricingSection data={pricing} />
     </>
   )
 }
