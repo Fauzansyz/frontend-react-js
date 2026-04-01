782d3dc4d5919abef6f6c00f30c5c87862552015
Fauzansyz <fauzanadzma2@gmail.com>
2026-04-01 13:28:49 +0000

    CI/CD push to gh

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/porto-ui-a/react-js-frontend/",
+  base: "/react-js-frontend/",
 });
