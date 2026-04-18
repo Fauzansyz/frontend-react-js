818b70afdd69948e82544c7630f1583c3381dca6
Fauzansyz <fauzanadzma2@gmail.com>
2026-04-18 15:59:34 +0000

    Nothinh hahah

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,118 @@
-import { useState, useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
-import './App.css'
-import TopNavBar from './components/navbar/TopNavBar'
-import HeroSection from './components/hero/HeroSection'
-import PricingSection from './components/pricing/PricingSection'
-import FeatureSection from './components/feature/FeatureSection'
-import TestimoniSection from './components/testimonial/TestimoniSection'
-import FAQSection from './components/faq/FAQSection'
+import "./App.css";
+import TopNavBar from "./components/navbar/TopNavBar";
+import HeroSection from "./components/hero/HeroSection";
+import PricingSection from "./components/pricing/PricingSection";
+import FeatureSection from "./components/feature/FeatureSection";
+import TestimoniSection from "./components/testimonial/TestimoniSection";
+import FAQSection from "./components/faq/FAQSection";
+import FooterSection from "./components/footer/FooterSection";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const ref = useRef(null)
-
   const FAQData = [
     {
-      question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.',
-      answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.'
+      question:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
     },
     {
-      question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.',
-      answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.'
+      question:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
     },
     {
-      question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.',
-      answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.'
+      question:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
     },
     {
-      question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.',
-      answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.'
-    }
-  ]
+      question:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
+    },
+  ];
 
   const userTestimonial = [
     {
-      name: 'John Doe',
-      profileImage: 'https://images.unsplash.com/photo-1614644147724-2d4785d69962?auto=format&fit=crop&q=80&w=1160',
-      category: 'CEO',
-      testimonial: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.',
+      name: "John Doe",
+      profileImage:
+        "https://images.unsplash.com/photo-1614644147724-2d4785d69962?auto=format&fit=crop&q=80&w=1160",
+      category: "CEO",
+      testimonial:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
     },
     {
-      name: 'AsepSurasep',
-      profileImage: 'https://images.unsplash.com/photo-1614644147724-2d4785d69962?auto=format&fit=crop&q=80&w=1160',
-      category: 'Founder',
-      testimonial: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.',
-    }
-  ]
+      name: "AsepSurasep",
+      profileImage:
+        "https://images.unsplash.com/photo-1614644147724-2d4785d69962?auto=format&fit=crop&q=80&w=1160",
+      category: "Founder",
+      testimonial:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
+    },
+  ];
 
   const pricing = [
     {
-    name: 'Child',
-    pricing: '$29',
-    feature: [
-      '20 users logins per month',
-      'Unlimited projects',
-      'Unlimited features',
-      'Unlimited support',
-      '1Gbps data transfer'
-      ]
+      name: "Child",
+      pricing: "$29",
+      feature: [
+        "20 users logins per month",
+        "Unlimited projects",
+        "Unlimited features",
+        "Unlimited support",
+        "1Gbps data transfer",
+      ],
     },
     {
-    name:"Pioneer",
-    pricing: '$49',
-    feature: [
-      '100 users logins per month',
-      'Unlimited projects',
-      'Unlimited features',
-      'Unlimited support',
-      '15Gbps data transfer'
-      ]
+      name: "Pioneer",
+      pricing: "$49",
+      feature: [
+        "100 users logins per month",
+        "Unlimited projects",
+        "Unlimited features",
+        "Unlimited support",
+        "15Gbps data transfer",
+      ],
     },
     {
-    name:"Explorer",
-    pricing: '$99',
-    feature: [
-      'Unlimited users logins per month',
-      'Unlimited projects',
-      'Unlimited features',
-      'Unlimited support',
-      'Unlimited data transfer'
-      ]
+      name: "Explorer",
+      pricing: "$99",
+      feature: [
+        "Unlimited users logins per month",
+        "Unlimited projects",
+        "Unlimited features",
+        "Unlimited support",
+        "Unlimited data transfer",
+      ],
     },
-     {
-    name:"Buisness",
-    pricing: '$199',
-    feature: [
-      'Unlimited users logins per month',
-      'Unlimited projects',
-      'Unlimited features',
-      'Unlimited support',
-      'Unlimited data transfer',
-      'Early access to new features'
-      ]
-     }
-  ]
+    {
+      name: "Buisness",
+      pricing: "$199",
+      feature: [
+        "Unlimited users logins per month",
+        "Unlimited projects",
+        "Unlimited features",
+        "Unlimited support",
+        "Unlimited data transfer",
+        "Early access to new features",
+      ],
+    },
+  ];
 
   return (
     <>
-    <TopNavBar />
-     <HeroSection />
+      <TopNavBar />
+      <HeroSection />
       <FeatureSection />
-     <PricingSection data={pricing} />
-    <TestimoniSection data={userTestimonial} />
-  <FAQSection data={FAQData} />
+      <PricingSection data={pricing} />
+      <TestimoniSection data={userTestimonial} />
+      <FAQSection data={FAQData} />
+      <FooterSection />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/footer/FooterSection.jsx
@@ -0,0 +1,35 @@
+export default function FooterSection() {
+  return (
+    <>
+      <div className="col-lg-3">
+    <div className="bg-gray-600 p-4">
+      <h4 className="widget-title">Contact Us</h4>
+      <ul className="contact-info">
+        <li>
+          <i className="icon icon-animated icon-map-marker"></i>
+          <p>
+            123 Street Name, City Name, United States
+            <br />
+            Zip 00000
+          </p>
+        </li>
+        <li>
+          <i className="icon icon-animated icon-phone"></i>
+          <p>
+            <a href="tel:+1234567890">+1 234 456 7890</a>
+            <br />
+            <a href="tel:+1234567890">+1 234 456 7890</a>
+          </p>
+        </li>
+        <li>
+          <i className="icon icon-animated icon-envelope"></i>
+          <a href="mailto:W2o9n@example.com">W2o9n@example.com</a>
+          <br />
+          <a href="mailto:W2o9n@example.com">W2o9n@example.com</a>
+        </li>
+      </ul>
+    </div>  
+    </div>;
+    </>
+  )
+}
